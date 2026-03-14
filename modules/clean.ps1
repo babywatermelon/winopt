9d4f5e2fa5ec90cef0c956f064d39ef04503a3a0
Baby Watermelon <triclgtyahoo@gmail.com>
2026-03-14 10:57:58 +0700

    Update clean.ps1

--- modules/clean.ps1
+++ modules/clean.ps1
@@ -1,1 +1,25 @@
+function Clean-Temp {
 
+Remove-Item "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Temp cleaned" -ForegroundColor Green
+
+}
+
+function Clean-Prefetch {
+
+Remove-Item "C:\Windows\Prefetch\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Prefetch cleaned" -ForegroundColor Green
+
+}
+
+function Clean-WindowsUpdate {
+
+Write-Host "Cleaning Windows Update Cache..." -ForegroundColor Yellow
+
+net stop wuauserv
+Remove-Item "C:\Windows\SoftwareDistribution\*" -Recurse -Force -ErrorAction SilentlyContinue
+net start wuauserv
+
+Write-Host "Completed" -ForegroundColor Green
+
+}
