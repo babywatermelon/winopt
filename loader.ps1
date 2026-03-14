f3f1b30f8ae5e24b6557ed983628362eabacc4d7
Baby Watermelon <triclgtyahoo@gmail.com>
2026-03-14 09:58:09 +0700

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -7,6 +7,6 @@ if(!(Test-Path $temp)){
 New-Item -ItemType Directory -Path $temp | Out-Null
 }
 
-Invoke-WebRequest https://raw.githubusercontent.com/USERNAME/winopt/main/menu.ps1 -OutFile "$temp\menu.ps1"
+Invoke-WebRequest https://raw.githubusercontent.com/babywatermelon/winopt/main/menu.ps1 -OutFile "$temp\menu.ps1"
 
 powershell "$temp\menu.ps1"
