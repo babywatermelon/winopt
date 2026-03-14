9dacb0fd3ba3c6912a3a4c204e24bad4f98df539
Baby Watermelon <triclgtyahoo@gmail.com>
2026-03-14 10:19:03 +0700

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -1,37 +1,39 @@
+while($true){
+
 Clear-Host
 
-Write-Host "====================="
-Write-Host "      WINOPT TOOL"
-Write-Host "====================="
+Write-Host "==== WINOPT TOOL ====" -ForegroundColor Cyan
+Write-Host ""
+Write-Host "[1] Clean Temp"
+Write-Host "[2] Network Reset"
+Write-Host "[3] Repair Windows"
+Write-Host "[0] Exit"
 
-Write-Host "1. Clean Temp"
-Write-Host "2. Network Reset"
-Write-Host "3. Repair Windows"
-Write-Host "0. Exit"
-
-$choice=Read-Host "Select"
+$choice = Read-Host "Select"
 
 switch($choice){
 
-1 {
+1{
 Remove-Item "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
-Write-Host "Temp cleaned"
+Write-Host "Temp cleaned" -ForegroundColor Green
 pause
 }
 
-2 {
+2{
 ipconfig /flushdns
 netsh winsock reset
 pause
 }
 
-3 {
+3{
 sfc /scannow
 pause
 }
 
-0 {
+0{
 exit
+}
+
 }
 
 }
