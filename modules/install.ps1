08efe47dd79b1c1f190fbbe441ec4b37bf999ce4
Baby Watermelon <triclgtyahoo@gmail.com>
2026-03-17 17:40:07 +0700

    Update install.ps1

--- modules/install.ps1
+++ modules/install.ps1
@@ -1,1 +1,17 @@
+function Install-Chrome {
 
+    Write-Host "Checking Chrome..." -ForegroundColor Cyan
+
+    $app = winget list --id Google.Chrome
+
+    if ($app -match "Google Chrome") {
+        Write-Host "Chrome already installed!" -ForegroundColor Yellow
+        return
+    }
+
+    Write-Host "Installing Chrome..." -ForegroundColor Cyan
+
+    winget install --id Google.Chrome --exact --accept-package-agreements --accept-source-agreements
+
+    Write-Host "Done!" -ForegroundColor Green
+}
