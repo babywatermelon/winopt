142d02f7a5f3f9c5eb72e44c8817c8b50a4a4116
Baby Watermelon <triclgtyahoo@gmail.com>
2026-03-17 17:22:42 +0700

    Update installer.ps1

--- modules/installer.ps1
+++ modules/installer.ps1
@@ -1,18 +1,3 @@
 function Install-Chrome {
-
-    Write-Host ""
-    Write-Host "Installing Google Chrome..." -ForegroundColor Yellow
-
-    try {
-        winget install -e --id Google.Chrome `
-        --silent `
-        --accept-package-agreements `
-        --accept-source-agreements
-
-        Write-Host "Chrome installed successfully!" -ForegroundColor Green
-    }
-    catch {
-        Write-Host "Install failed!" -ForegroundColor Red
-    }
-
+    winget install Google.Chrome --accept-package-agreements --accept-source-agreements
 }
