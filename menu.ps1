2451c5e9b376db7995b02d1d71c71fc67dc88fca
Baby Watermelon <triclgtyahoo@gmail.com>
2026-03-28 03:51:21 +0700

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -86,7 +86,7 @@ function Draw-Section {
 function Show-Menu {
     Header
     $width = $Host.UI.RawUI.WindowSize.Width
-    $menuWidth = 82
+    $menuWidth = 90
     $leftPadding = [math]::Max(0, [math]::Floor(($width - $menuWidth) / 2))
 
     Write-Host (" " * $leftPadding + "+" + ("-" * ($menuWidth - 2)) + "+") -ForegroundColor DarkGray
